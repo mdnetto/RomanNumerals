c0b6d0d825b148afc3945f075c70a193908a9986
Michelle D'Netto <mdnetto@4137.local>
2016-09-01 11:36:30 -0400

    Finished

--- roman_numerals.js
+++ roman_numerals.js
@@ -7,10 +7,9 @@
 // greater value subtracts (thus XC or xc is 90).
 //
 // I, II, III, IV, V, VI, VII, VIII, IX, X
-// XI, XII, XIII, XIV, XV, XVI, 
+// XI, XII, XIII, XIV, XV, XVI, XVII,  
 //
 const readline = require('readline');
-
 const read = readline.createInterface({
 	  input: process.stdin,
 	  output: process.stdout
@@ -18,42 +17,49 @@ const read = readline.createInterface({
 
 function getNumber() {
 	read.question('Type in a number to be converted to a Roman Numeral: ', (number) => {
+		var romanNumeral = '';
 		console.log('The Roman Numeral is: ', checkForMatch(number));
 		read.close();
 	});
 }
 
 function checkForMatch(n) {
-var romanNumerals = { 1:"I", 5:"V", 10:"X", 50:"L", 100:"C", 500:"D", 1000:"M" };
-	var array = Object.keys(romanNumerals);
+var rn = { 1:"I", 5:"V", 10:"X", 50:"L", 100:"C", 500:"D", 1000:"M" };
+	var array = Object.keys(rn);
 	for (var i=0; i<=array.length; i++){
 		if (n == array[i]) {
-			return romanNumerals[array[i]];
-		 } else {
-			 convertOnes(n);
+			return rn[array[i]];
+		} else if (n.length == 1) {
+			return convert(n, 'I', 'V', 'X');
+		} else if (n.length == 2) {
+			return convert(n[0], 'X', 'L', 'C') + convert(n[1], 'I', 'V', 'X');
+		} else if (n.length == 3) {
+			return convert(n[0], 'C', 'D', 'M') + convert(n[1], 'X', 'L', 'C') + convert(n[2], 'I', 'V', 'X');
 		}
 	}
+	return false;
 }
 
-function convertOnes(n) {
-	if (romanNumerals[1]) {
-	var string = "";
-		if (n < 4 && n < 8) {
-			string += "V";
-			if (n-5 != 0) {
-				buildSingles(n-5, string);
-			}
-		}
+function convert(n, unit, min, max) {
+	var string = '';
+	if (n <= 3) {
+		string = buildSingles(n, unit);
+	} else if (n == 4) {
+	  string += unit + min; 
+	} else if (n == 9) {
+	  string += unit + max;
+	} else if (n <= 8) {
+	  string += min + buildSingles(n-5, unit); 
 	}
+	return string;
+}
+
+function buildSingles(n, unit) {
+	var string = '';
+		for (var i=1; i<=n; i++) {
+			string += unit; 
+		}
+	return string;
 }
 
 getNumber();
-
-function buildSingles(n, string) {
-	typeof string === 'undefined' ? string = '' : string;
-	if (n <= 3) {
-		for (var i=1; i<=n; i++) {
-			string += "I";
-		}
-	}
-}
